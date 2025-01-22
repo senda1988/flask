25a586b3f4c24fbc4b79bc08eb6f6282c87559bb
senda1988 <zidi.senda@gmail.com>
2025-01-22 15:44:49 +0100

    Create users.py

---
+++ classes/users.py
@@ -0,0 +1,26 @@
+users = [
+    {
+        "id": 1,
+        "name": "Alice",
+        "password": "123",
+        "username": "Useralice",
+        "email": "alice@example.com",
+        "age": 22,
+    },
+    {
+        "id": 2,
+        "name": "Bob",
+        "password": "456",
+        "username": "UserBob",
+        "email": "bob@example.com",
+        "age": 30,
+    },
+    {
+        "id": 3,
+        "name": "Charlie",
+        "password": "789",
+        "username": "UserCharlie",
+        "email": "charlie@example.com",
+        "age": 40,
+    },
+]
